005172a6edaff4cb4314b50ab459cc7e6f3332c2
jiurim <78469127+jiurim@users.noreply.github.com>
2024-02-06 22:52:14 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 884 ms, Memory: 249308 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i=0;i<t;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            stringBuilder.append(a+b+"\n" );
+        }
+        System.out.println(stringBuilder);
+
+
+    }
+}
