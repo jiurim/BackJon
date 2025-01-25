bd8cd397c382d6c7d9320fb1d6be44a006bec32b
jiurim <78469127+jiurim@users.noreply.github.com>
2025-01-25 16:15:00 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 600 ms, Memory: 294816 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException{
-    
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-        
+        String [] index = bf.readLine().split(" ");
+        int N = Integer.parseInt(index[0]);
+        int K = Integer.parseInt(index[1]);
+
         Queue<Integer> queue = new LinkedList<>();
-
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int K = Integer.parseInt(stringTokenizer.nextToken());
-        
-        for (int i = 1; i <= N; i++) {
+        for(int i=1;i<=N;i++){
             queue.add(i);
         }
-        
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < K - 1; i++) {
-                queue.offer(queue.poll());
+
+        StringBuilder result = new StringBuilder();
+        result.append("<");
+
+        while(!queue.isEmpty()){
+            for(int i=0;i<K-1;i++){
+                queue.add(queue.remove());
             }
-            linkedList.add(queue.poll());
-        }
-        
-        System.out.print("<");
-        for (int i = 0; i < linkedList.size(); i++) {
-            if (i == linkedList.size() - 1) {
-                System.out.print(linkedList.get(i));
-            } else {
-                System.out.print(linkedList.get(i) + ", ");
+            result.append(queue.remove());
+            if(!queue.isEmpty()){
+                result.append(", ");
             }
         }
-        System.out.print(">");
+
+        result.append(">");
+        System.out.println(result);
     }
+    
 }
